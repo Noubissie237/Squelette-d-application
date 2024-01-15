99fda8515ed2be56069b4fe73620e77983e37ab2
Noubissie237 <noubissiekamgawilfried2017@gmail.com>
2024-01-15 23:21:42 +0100

    Ajoutez des fichiers projet.

---
+++ Squelette/Squelette/Views/CountPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Squelette.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CountPage : ContentPage
+    {
+        public CountPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
